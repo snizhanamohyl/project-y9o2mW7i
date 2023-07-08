50b249b13fdd057f9ed6647735412bbad4a34915
helenserb <serbinenkoifbb@gmail.com>
2023-07-08 19:35:44 -0400

    Create AuthNav.jsx
    authnav markup

---
+++ src/components/AuthNav/AuthNav.jsx
@@ -0,0 +1,8 @@
+export default function AuthNav() {
+    return (
+        <BtnWrapper>
+            <SignUpBtn>Registration</SignUpBtn>
+            <SignInBtn>SignIn</SignInBtn>
+        </BtnWrapper>
+    );
+}
