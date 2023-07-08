e646dd137c860d05b632bef342a7577aa1d2023d
helenserb <serbinenkoifbb@gmail.com>
2023-07-08 11:19:32 -0400

    Update WelcomePage.jsx
    Added Greetings + AuthNav components

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,3 +1,12 @@
+import React from "react"
+import { Greetings } from 'components/Greetings/Greetings';
+import { AuthNav } from 'components/AuthNav/AuthNav';
+
 export default function WelcomePage() {
-    return <h1>WelcomePage</h1>
+    return (
+      <>
+            <Greetings />
+            <AuthNav/>
+      </>
+    );
 }
