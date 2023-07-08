0a60dba9ba5575337dcce876c738d16e9675ce28
helenserb <serbinenkoifbb@gmail.com>
2023-07-08 11:46:50 -0400

    Greeting

---
+++ src/components/Greeting/Greeting.jsx
@@ -0,0 +1,17 @@
+export default function Greeting() { 
+    return (
+      <div>
+        {/* <a href="./index.html">
+          <svg class="icon logo__icon">
+            <use href="./images/header.svg#icon-logo"></use>
+          </svg>
+        </a> */}
+        <h1>Welcome to the app!</h1>
+        <p>
+          This app offers more than just a collection of recipes - it is
+          designed to be your very own digital cookbook. You can easily save and
+          retrieve your own recipes at any time.
+        </p>
+      </div>
+    );
+}
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,12 +1,12 @@
 import React from "react"
-import { Greetings } from 'components/Greetings/Greetings';
-import { AuthNav } from 'components/AuthNav/AuthNav';
+import Greeting from 'components/Greeting/Greeting';
+// import { AuthNav } from 'components/AuthNav/AuthNav';
 
 export default function WelcomePage() {
     return (
       <>
-            <Greetings />
-            <AuthNav/>
+            <Greeting />
+            {/* <AuthNav/> */}
       </>
     );
 }
