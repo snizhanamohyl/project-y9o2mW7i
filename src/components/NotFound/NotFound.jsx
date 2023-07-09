99c1edeff7223ea05b15eca6cdfe617b7a66fb3a
helenserb <serbinenkoifbb@gmail.com>
2023-07-09 18:01:57 -0400

    change name of err images

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,11 +4,11 @@ export default function NotFound() {
         <ErrorThumb>
           <ErrorPic>
             <source
-              srcset="./images/igor-desktop.jpg 1x,./images/igor-desktop@2x.jpg 2x" media="(min-width: 1440px)"/>
+              srcset="./images/err.jpg 1x,./images/err@2x.jpg 2x" media="(min-width: 1440px)"/>
             <source
-              srcset="./images/igor-tablet.jpg 1x, ./images/igor-tablet@2x.jpg 2x" media="(min-width: 768px)"/>
+              srcset="./images/errTab.jpg 1x, ./images/errTab@2x.jpg 2x" media="(min-width: 768px)"/>
             <img
-              srcset="./images/igor-mobile 1x, ./images/igor-mobile@2x.jpg 2x" src="./images/igor-mobile.jpg" alt="Error"/>
+              srcset="./images/errMob 1x, ./images/ErrMob@2x.jpg 2x" src="./images/errMob.jpg" alt="Error"/>
           </ErrorPic>
           <ErrorTitle>We are sorry,</ErrorTitle>
           <ErrorText>
