3ab3428aa42b23f0d2151d32e044b784f45b2b1f
helenserb <serbinenkoifbb@gmail.com>
2023-07-09 17:59:25 -0400

    change tag img to picture

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -2,9 +2,18 @@ export default function NotFound() {
     return (
       <ErrorWrapper>
         <ErrorThumb>
-          <img src={error} alt="error" className={style.errorImage} />
+          <ErrorPic>
+            <source
+              srcset="./images/igor-desktop.jpg 1x,./images/igor-desktop@2x.jpg 2x" media="(min-width: 1440px)"/>
+            <source
+              srcset="./images/igor-tablet.jpg 1x, ./images/igor-tablet@2x.jpg 2x" media="(min-width: 768px)"/>
+            <img
+              srcset="./images/igor-mobile 1x, ./images/igor-mobile@2x.jpg 2x" src="./images/igor-mobile.jpg" alt="Error"/>
+          </ErrorPic>
           <ErrorTitle>We are sorry,</ErrorTitle>
-          <ErrorText>but the page you were looking for can’t be found..</ErrorText>
+          <ErrorText>
+            but the page you were looking for can’t be found..
+          </ErrorText>
         </ErrorThumb>
       </ErrorWrapper>
     );
