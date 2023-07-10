5e4c8073c7a4e581bc88ff25a415218e326f3780
Mariia Khapun <111394270+AlluringUnicorn@users.noreply.github.com>
2023-07-10 18:27:57 +0300

    added redux auth

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,38 @@
-console.log('store');
+import { authReducer } from './auth/authSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+export const rootReducer = combineReducers({
+  auth: persistedReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
