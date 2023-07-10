a87f0127ad12c547215a7e6ee1cc4002757e14ed
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-07-10 20:05:18 +0300

    add: styles, logic for reusing SearchForm component

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Input } from './SearchForm.styled';
+import { useLocation } from 'react-router';
 
-export default function SearchForm({ onType }){
-  const handleChangeQueue = e => onType(e.target.value);
+export default function SearchForm() {
+  const [query, setQuery] = useState('');
+
+  const { pathname } = useLocation();
+  const isSearchPage = pathname.includes('/search');
+
+  const handleChangeQueue = e => setQuery(e.target.value);
+  
   const handleSubmit = e => {
     e.preventDefault();
     console.log('submitted');
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} issearchpage={isSearchPage.toString()}>
       <Input
         id="search"
         name="search"
-        placeholder="salad"
+        placeholder="Enter the text"
         type="text"
         onChange={handleChangeQueue}
+        value={query}
       />
-      <Button type="submit">Search</Button>
+      <Button type="submit" issearchpage={isSearchPage.toString()}>Search</Button>
     </Form>
   );
 };
--- src/components/SearchForm/SearchForm.styled.js
+++ src/components/SearchForm/SearchForm.styled.js
@@ -1,66 +1,78 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
+  ${issearchpage =>
+    issearchpage === 'true'
+      ? `  margin-right: auto;
+           margin-left: auto;`
+      : ''};
   width: 295px;
+  height: 52px;
   display: flex;
   align-items: center;
-  background-color: var(--white);
   border: 1px solid var(--input-border-color);
   border-radius: 54px 104px 54px 104px;
+  background-color: var(--white);
   filter: drop-shadow(0px 4px 97px rgba(34, 37, 42, 0.03));
+  transition: border-color var(--transition-time) var(--transition-function);
 
-  &:focus-within {
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  }
-  &:focus-within button {
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  &:focus-within,
+  &:hover,
+  &:focus {
+    border-color: rgba(35, 38, 42, 0.2);
   }
 
   @media (min-width: 768px) {
     width: 362px;
+    height: 59px;
   }
 
   @media (min-width: 1440px) {
     width: 510px;
+    height: 70px;
   }
 `;
 
 export const Input = styled.input`
   display: block;
   width: 100%;
+  padding: 17px 32px;
   font-size: 12px;
   line-height: normal;
-  color: var(--placeholder-color);
-  padding: 17px 32px;
+  color: var(--text-primary);
   outline: none;
   border: none;
   background-color: transparent;
 
+  &::placeholder {
+    color: var(--placeholder-color);
+  }
+
   @media (min-width: 768px) {
     font-size: 16px;
-    padding: 18px 38px;
+    padding: 17px 38px;
   }
 
   @media (min-width: 1440px) {
     padding: 23px 48px;
   }
-  /* &:-internal-autofill-selected {
-    background-color: var(--bg-color) !important;
-  } */
 `;
 
 export const Button = styled.button`
-  font-size: 16px;
+  height: calc(100% + 2px);
   padding: 16px 32px;
+  font-size: 14px;
   line-height: normal;
   color: var(--bg-color);
-  background-color: var(--dark-accent);
+  background-color: ${issearchpage =>
+    issearchpage === 'true' ? 'var(--accent)' : 'var(--dark-accent)'};
   border-radius: 54px 104px 54px 104px;
   transition: background-color var(--transition-time) var(--transition-function);
 
   &:hover,
   &:focus {
-    background-color: var(--accent);
+    background-color: ${issearchpage =>
+      issearchpage === 'true' ? 'var(--dark-accent)' : 'var(--accent)'};
   }
 
   @media (min-width: 768px) {
