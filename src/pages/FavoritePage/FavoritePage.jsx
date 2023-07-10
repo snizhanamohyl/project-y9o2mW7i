6f9fafd5f5761a868b0d79950aaf82e33aeefd43
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-07-10 10:19:53 +0300

    Update FavoritePage.jsx

--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,3 +1,62 @@
-export default function FavoritePage() {
-    return <h1>FavoritePage</h1>
-}
+import React from 'react';
+import RecipeCard from './RecipeCard';
+import Pagination from './Pagination';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Favorite = () => {
+    const [recipes, setRecipes] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [recipesPerPage] = useState(4);
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+        try {
+            const response = await axios.get('https://64a8b750dca581464b85f54e.mockapi.io/recipes');
+            setRecipes(response.data);
+        } catch (error) {
+            console.error('Error fetching recipes:', error);
+        }
+    };
+
+    fetchRecipes();
+    }, []);
+
+
+    // індекс останнього рецепту на поточній сторінці
+    const lastRecipeIndex = currentPage * recipesPerPage;
+    //індекс першого рецепту на поточній сторінці
+    const firstRecipeIndex = lastRecipeIndex - recipesPerPage;
+    //масив рецептів для поточної сторінки
+    const currentRecipes = recipes.slice(firstRecipeIndex, lastRecipeIndex);
+
+    
+    const pagination = pageNumber => setCurrentPage(pageNumber);
+    const nextPage = () => {setCurrentPage( prev => prev + 1)}
+    const prevPage = () => {setCurrentPage( prev => prev - 1)}
+
+
+  return (
+
+    <div>
+        <h1>Favorite </h1>
+        {recipes.length > 0 ? (
+            <RecipeCard recipe={currentRecipes}/>
+        ):(
+            <p>Улюблених рецептів немає</p>
+        )}
+
+        {/* Pagination */}
+        <Pagination 
+            recipesPerPage={recipesPerPage} 
+            totalRecipe={recipes.length} 
+            pagination={pagination}
+            nextPage={nextPage}
+            prevPage={prevPage}
+            currentPage={currentPage}
+        />
+    </div>
+  );
+};
+
+export default Favorite;
