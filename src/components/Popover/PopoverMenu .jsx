58be3f6e09a2ff0dcd5e6fe455c867defa6da2b7
helenserb <serbinenkoifbb@gmail.com>
2023-07-11 02:13:59 -0400

    styles logoutbtn on popover

--- src/components/Popover/PopoverMenu .jsx
+++ src/components/Popover/PopoverMenu .jsx
@@ -1,19 +1,18 @@
 import { EditLink, LogOutBtn } from './PopoverMenu.styled'
-import sprite from '../../assets/sprite.svg';
-
-
-
+import sprite from '../../assets/sprite.svg'
 
 export default function PopoverMenu() {
   return (
     <>
       <EditLink>
         <p>Edit profile</p>
-        <svg>
-          <use href={sprite + '#icon-arrow-right'}></use>
-        </svg>
       </EditLink>
-      <LogOutBtn>Log out</LogOutBtn>
+      <LogOutBtn>
+        Log out
+        <svg width="18" height="18">
+          <use href={`${sprite}#icon-arrow-right`}></use>
+        </svg>
+      </LogOutBtn>
     </>
   );
 }
--- src/components/Popover/PopoverMenu.styled.js
+++ src/components/Popover/PopoverMenu.styled.js
@@ -5,19 +5,23 @@ display: block;
 `;
 
 export const LogOutBtn = styled.a`
- display: block;
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 4px;
  line-height: 1.2;
+
   background-color: var(--accent);
   padding: 12px 24px;
   border-radius: 24px 44px;
-  border: 2px solid var(--bg-color);
+  border: 0px solid var(--bg-color);
   color: var(--bg-color);
   cursor: pointer;
 
   &:hover,
   &:focus {
     background-color: var(--dark-accent);
-    border: 2px solid var(--dark-accent);
+    border: 0px solid var(--dark-accent);
     transition: background-color, border, var(--transition-time) var(--transition-function);
   }
 
