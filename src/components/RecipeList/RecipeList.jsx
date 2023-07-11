7bda44be991e2a135c7f7842abaf5afedcc585ec
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-07-11 18:22:09 +0300

    add: services folder

---
+++ src/components/RecipeList/RecipeList.jsx
@@ -0,0 +1,13 @@
+export default function PriviewCategories() {
+    return <ul>
+            <RecipeList>
+                <h2></h2>
+                <ul>{ }</ul> 
+            </RecipeList>
+            <li>breakfastSection</li>
+            <li>breakfastSection</li>
+        </ul>
+}
+
+
+
