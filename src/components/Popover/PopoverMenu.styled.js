81481677a249ce34638d0a1caf9206793e378d10
helenserb <serbinenkoifbb@gmail.com>
2023-07-11 12:01:11 -0400

    remove border styles
    + replace transition

--- src/components/Popover/PopoverMenu.styled.js
+++ src/components/Popover/PopoverMenu.styled.js
@@ -11,29 +11,27 @@ export const EditLink = styled.a`
   line-height: 1.6;
 
   @media screen and (min-width: 768px) {
-    gap: 53px;
+  gap: 53px;
   }
 `;
 
 export const LogOutBtn = styled.a`
-display: flex;
-align-items: center;
-justify-content: center;
-gap: 4px;
- line-height: 1.2;
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  line-height: 1.2;
   background-color: var(--accent);
   padding: 12px 24px;
   border-radius: 24px 44px;
-  border: 0px solid var(--bg-color);
+  border: none;
   color: var(--bg-color);
   cursor: pointer;
+  transition: background-color, var(--transition-time) var(--transition-function);
 
   &:hover,
   &:focus {
     background-color: var(--dark-accent);
-    border: 0px solid var(--dark-accent);
-    transition: background-color, border, var(--transition-time) var(--transition-function);
   }
 
 `;
