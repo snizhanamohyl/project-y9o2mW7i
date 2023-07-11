5c269337aeeecdaef48ce9f99cdd8869bd4a3882
Nataliia Semenova <natasemenova1806@gmail.com>
2023-07-11 15:32:09 +0300

    add pull

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,23 @@
-<<<<<<< HEAD
 import { Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
-=======
-import { Route, Routes } from "react-router-dom";
-import { lazy, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { refreshUser } from "redux/auth/auth-operations";
->>>>>>> main
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { refreshUser } from 'redux/auth/auth-operations';
 
 import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
 import RestrictedRoute from 'components/RestrictedRoute/RestrictedRoute';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 
-import RegisterPage from "pages/RegisterPage/RegisterPage";
-import SigninPage from "pages/SigninPage/SigninPage";
+import RegisterPage from 'pages/RegisterPage/RegisterPage';
+import SigninPage from 'pages/SigninPage/SigninPage';
 
 const MainPage = lazy(() => import('pages/MainPage/MainPage'));
-<<<<<<< HEAD
-const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
-const SigninPage = lazy(() => import('pages/SigninPage/SigninPage'));
+
+// const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+// const SigninPage = lazy(() => import('pages/SigninPage/SigninPage'));
 const CategoriesPage = lazy(() =>
   import('pages/CategoriesPage/CategoriesPage')
 );
-=======
-// const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
-// const SigninPage = lazy(() => import('pages/SigninPage/SigninPage'));
-const CategoriesPage = lazy(() => import('pages/CategoriesPage/CategoriesPage'));
->>>>>>> main
+
 const MyRecipesPage = lazy(() => import('pages/MyRecipesPage/MyRecipesPage'));
 const RecipePage = lazy(() => import('pages/RecipePage/RecipePage'));
 const AddRecipePage = lazy(() => import('pages/AddRecipePage/AddRecipePage'));
@@ -37,17 +28,27 @@ const ShoppingListPage = lazy(() =>
 const FavoritePage = lazy(() => import('pages/FavoritePage/FavoritePage'));
 const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
 
-export default function App() {  
-
+export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return <Routes>
-    <Route path='/signin' element={<RestrictedRoute component={SigninPage} redirectTo='/signin'/>}></Route>
-    <Route path='/register' element={<RestrictedRoute component={RegisterPage} redirectTo='/register' />}></Route>
+  return (
+    <Routes>
+      <Route
+        path="/signin"
+        element={
+          <RestrictedRoute component={SigninPage} redirectTo="/signin" />
+        }
+      ></Route>
+      <Route
+        path="/register"
+        element={
+          <RestrictedRoute component={RegisterPage} redirectTo="/register" />
+        }
+      ></Route>
 
       <Route path="/" element={<SharedLayout />}>
         <Route
