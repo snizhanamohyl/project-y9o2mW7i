daaf1b76644dbfe841b8270bacb50fc2075ee810
helenserb <serbinenkoifbb@gmail.com>
2023-07-11 01:33:17 -0400

    style logout btn

--- src/components/Popover/PopoverMenu .jsx
+++ src/components/Popover/PopoverMenu .jsx
@@ -1,8 +1,18 @@
 import { EditLink, LogOutBtn } from './PopoverMenu.styled'
+import sprite from '../../assets/sprite.svg';
+
+
+
+
 export default function PopoverMenu() {
   return (
     <>
-      <EditLink>Edit profile</EditLink>
+      <EditLink>
+        <p>Edit profile</p>
+        <svg>
+          <use href={sprite + '#icon-arrow-right'}></use>
+        </svg>
+      </EditLink>
       <LogOutBtn>Log out</LogOutBtn>
     </>
   );
--- src/components/Popover/PopoverMenu.styled.js
+++ src/components/Popover/PopoverMenu.styled.js
@@ -5,5 +5,21 @@ display: block;
 `;
 
 export const LogOutBtn = styled.a`
-display: block;
+ display: block;
+ line-height: 1.2;
+  background-color: var(--accent);
+  padding: 12px 24px;
+  border-radius: 24px 44px;
+  border: 2px solid var(--bg-color);
+  color: var(--bg-color);
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    background-color: var(--dark-accent);
+    border: 2px solid var(--dark-accent);
+    transition: background-color, border, var(--transition-time) var(--transition-function);
+  }
+
 `;
+
