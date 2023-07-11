51272d2925fdb5f5e58145ecf99a38fd19a11f0c
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-07-11 09:21:07 +0300

    changes MyrRecipePage

--- src/pages/MyRecipesPage/MyRecipesPage.jsx
+++ src/pages/MyRecipesPage/MyRecipesPage.jsx
@@ -1,3 +1,57 @@
-export default function MyRecipesPage() {
-    return <h1>MyRecipesPage</h1>
-}
+import React from 'react';
+import RecipeCard from '../../components/RecipeCard/RecipeCard';
+import Pagination from '../../components/Pagination/Pagination';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { MyRecipesPage } from './MyRecipesPage.styled.jsx';
+
+const MyRecipesPage = () => {
+    const [recipes, setRecipes] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const recipesPerPage = 4;
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+        try {
+            const response = await axios.get('https://64a8b750dca581464b85f54e.mockapi.io/recipes');
+            setRecipes(response.data);
+        } catch (error) {
+            console.error('Error fetching recipes:', error);
+        }
+    };
+
+    fetchRecipes();
+    }, []);
+
+
+    // індекс останнього рецепту на поточній сторінці
+    const lastRecipeIndex = currentPage * recipesPerPage;
+    //індекс першого рецепту на поточній сторінці
+    const firstRecipeIndex = lastRecipeIndex - recipesPerPage;
+    //масив рецептів для поточної сторінки
+    const currentRecipes = recipes.slice(firstRecipeIndex, lastRecipeIndex);
+
+
+    return (
+
+        <div>
+            <MyRecipesPage>My recipes</MyRecipesPage>
+            {recipes.length > 0 ? (
+                <RecipeCard recipe={currentRecipes}/>
+            ):(
+                <p>Улюблених рецептів немає</p>
+            )}
+    
+            {/* Pagination */}
+            <Pagination 
+                recipesPerPage={recipesPerPage} 
+                totalRecipe={recipes.length} 
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
+        </div>
+      );
+    };
+    
+    export default MyRecipesPage;
---
+++ src/pages/MyRecipesPage/MyRecipesPage.styled.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const MyRecipesPage = styled.h1`
+    color: #001833;
+    font-family: Poppins;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 28px;
+    letter-spacing: -0.56px;
+
+    margin-bottom: 50px;
+
+    @media screen and (min-width: 767px){
+        font-size: 32px;
+        line-height: 32px;
+        letter-spacing: -0.64px;
+
+        margin-bottom: 100px;
+    }
+
+    @media screen and (min-width: 1439px){
+        font-size: 44px;
+        line-height: 44px;
+        letter-spacing: -0.88px;
+    }
+`
