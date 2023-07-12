92257cc19f7b8611c53127905ce2f1e03a5f1e9c
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-07-12 20:31:27 +0300

    block restricted route

--- src/components/RestrictedRoute/RestrictedRoute.jsx
+++ src/components/RestrictedRoute/RestrictedRoute.jsx
@@ -1,13 +1,15 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { Navigate } from 'react-router-dom';
 // import { selectToken } from 'redux/selectors';
 
 export default function RestrictedRoute({
   component: Component,
-  redirectTo = '/',
+  // redirectTo = '/',
 }) {
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  // const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
-  return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
+  // return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
+
+  return <Component />;
 
 }
