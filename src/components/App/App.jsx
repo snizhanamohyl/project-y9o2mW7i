1f2e167e130b916cd8248e01a5dfaf9c1a311cad
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-07-12 19:11:51 +0300

    add: key to li navigation elements, route for categories

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,6 +35,7 @@ export default function App() {
 
     <Route path='/' element={<SharedLayout />} >
       <Route index element={<PrivateRoute component={MainPage} redirectTo='/' />}></Route>
+      <Route path='/categories' element={<PrivateRoute component={CategoriesPage } redirectTo='/categories' />}></Route>
       <Route path='/categories/:categoryName' element={<PrivateRoute component={CategoriesPage } redirectTo='/categories/:categoryName' />}></Route>
       <Route path='/add' element={<PrivateRoute component={AddRecipePage} redirectTo='/add' />}></Route>
       <Route path='/my' element={<PrivateRoute component={MyRecipesPage} redirectTo='/my' />}></Route>
