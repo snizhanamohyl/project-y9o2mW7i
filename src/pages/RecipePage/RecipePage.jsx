df03814927c33d390df6eb2305e4e51382e9b331
Nataliia Semenova <natasemenova1806@gmail.com>
2023-07-13 07:22:03 +0300

    token

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -8,6 +8,8 @@ import SharedContainer from 'components/SharedContainer/SharedContainer';
 export default function RecipePage() {
   const { recipeId } = useParams();
   const [recipe, setRecipe] = useState([]);
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWY3OWMyMjYzNThlZjQxOGNkNjFiMyIsImlhdCI6MTY4OTIyMTU3MCwiZXhwIjoxNjg5MzAwNzcwfQ.Ivc09dx8jurySQ3rE78wDnTdboHTC0s9_yt8-0d4Xm0';
 
   return (
     <>
