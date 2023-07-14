3bb72960336dd124cd04d8b9ba19207f6eec558b
Nataliia Semenova <natasemenova1806@gmail.com>
2023-07-15 00:32:32 +0300

    selectors for shoping list

--- src/redux/ShopingList/selectors.js
+++ src/redux/ShopingList/selectors.js
@@ -1,6 +1,8 @@
 export const shopingList = state => state.shopingList;
 const checkedIngredientsList = state => {
-  const completedTodos = state.ingradients.filter(ingredient => ingredient.cheed)
-  return completedTodos.length
-}
-export const checkedIngredient = state => state.ingredients.checked
+  const checkedIngredientsList = state.ingradients.filter(
+    ingredient => ingredient.cheed
+  );
+  return checkedIngredientsList;
+};
+export const checkedIngredient = state => state.ingredients.checked;
