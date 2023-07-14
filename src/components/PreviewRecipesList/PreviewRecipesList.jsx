2dfbccaad389875c394536ff57279b9bcc8a01a4
VitaliyYakovenko <yakovenkovitaly1@gmail.com>
2023-07-15 00:25:55 +0300

    add fix width and styles btn

--- src/components/PreviewRecipesList/PreviewRecipesList.jsx
+++ src/components/PreviewRecipesList/PreviewRecipesList.jsx
@@ -4,31 +4,19 @@ import SeeAllBtn from "components/SeeAllBtn/SeeAllBtn";
 import { RecipeItem } from "./PreviewRecipesList.styled";
 import RecipesList from "../RecipesList/RecipesList";
 import getAllRecipes from "../../services/getAllRecipes";
-import useWindowWidth from "../../hooks/useWindowWidth";
 import processData from "../../services/processData";
 
 export default function PreviewRecipesList() {
    const [data, setData] = useState([]);
-   const width = useWindowWidth();
+   const width = window.innerWidth;
+   const pageLimit = width < 768 ? 1 : (width >= 1440 ? 4 : 2)     
+
 
    useEffect(() => {
     
-
-    if (width > 0 && width <= 767) {
-        
-           getAllRecipes(1).then(resp => setData(resp));
-           
-    } else if (width >= 768 && width <= 1439) {
-           
-           getAllRecipes(2).then(resp => setData(resp));
-           
-    } else if (width >= 1440) {
-           getAllRecipes(4).then(resp => setData(resp));
-        
-    };
-
-     
-   }, [width]);
+   getAllRecipes(pageLimit).then(resp => setData(resp));
+          
+   }, [pageLimit]);
    
    const recipes = processData(data?.recipes);
     
