3a0b8bc5aa27cd90ec2056d7504ed79027d86454
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-07-16 02:17:05 +0300

    fix: searchList render, token usage

--- src/services/search-api.js
+++ src/services/search-api.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://so-yummy-backend-hg4e.onrender.com/api/';
+// axios.defaults.baseURL = 'https://so-yummy-backend-hg4e.onrender.com/api/';
 
-const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-  unset() {
-    axios.defaults.headers.common.Authorization = '';
-  },
-};
+// const token = {
+//   set(token) {
+//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//   },
+//   unset() {
+//     axios.defaults.headers.common.Authorization = '';
+//   },
+// };
 
 const searchProducts = async (type, query) => {
-  const storage = localStorage.getItem('persist:auth');
-  const parsedStorage = JSON.parse(storage);
-  const persistToken = parsedStorage.token.replace(/"/g, '');
+  // const storage = localStorage.getItem('persist:auth');
+  // const parsedStorage = JSON.parse(storage);
+  // const persistToken = parsedStorage.token.replace(/"/g, '');
 
-  if (parsedStorage.token === null) {
-    return console.log('No token');
-  }
-  token.set(persistToken);
+  // if (parsedStorage.token === null) {
+  //   return console.log('No token');
+  // }
+  // token.set(persistToken);
 
   try {
     const requestType = type === 'query' ? 'search' : 'search/ingredients';
