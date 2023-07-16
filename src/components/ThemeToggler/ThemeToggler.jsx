b0b64359bbfede85c229f9d1f8afe42a40434d6c
uvchuk <uvchuk@gmail.com>
2023-07-16 17:01:38 +0200

    add class togler func

--- src/components/ThemeToggler/ThemeToggler.jsx
+++ src/components/ThemeToggler/ThemeToggler.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
-import { Switch, SwitchBody } from "./ThemeToggler.styled";
+import { useState } from 'react';
+import { Switch, SwitchBody } from './ThemeToggler.styled';
 
 export default function ThemeToggler({ customerStyles }) {
-    const [colorMode, setColorMode] = useState('light');
+  const [colorMode, setColorMode] = useState('light');
 
-    const toggleMode = () => {
-        setColorMode(colorMode ===  'light' ? 'dark' : 'light')
+  const toggleMode = () => {
+    setColorMode(colorMode === 'light' ? 'dark' : 'light');
+    if (colorMode === 'light') {
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
     }
+  };
 
-    return <SwitchBody onClick={toggleMode} $customerStyles={customerStyles}>
-        <Switch mode={colorMode}/>
+  return (
+    <SwitchBody onClick={toggleMode} $customerStyles={customerStyles}>
+      <Switch mode={colorMode} />
     </SwitchBody>
+  );
 }
