22f411e25faf49e94b3b1bbc17f34eddc4e275e8
helenserb <serbinenkoifbb@gmail.com>
2023-07-16 14:34:41 -0400

    Update UserLogo.jsx

--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Popover } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Avatar, AvatarImg, UserName, UserWrap } from './UserLogo.styled';
 import PopoverMenu from 'components/Popover/PopoverMenu ';
 import useWindowWidth from "hooks/useWindowWidth";
+import { getUser } from 'redux/auth/selectors';
 
 const theme = createTheme({
   components: {
@@ -21,6 +23,7 @@ const theme = createTheme({
 export default function UserLogo () {
   const width = useWindowWidth();
   const avatarSize = width < 768 ? '34px' : '44px';
+  const user = useSelector(getUser);
 
   const [anchorEl, setAnchorEl] = useState(null);
   const handleClick = event => { setAnchorEl(event.currentTarget); };
@@ -34,13 +37,13 @@ export default function UserLogo () {
         <UserWrap onClick={handleClick}>
           <Avatar>
             <AvatarImg
-              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww&w=1000&q=80"
-              alt="Olena"
+              src={!user.avatarURL ? "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww&w=1000&q=80" : user.avatarURL}
+              alt="User avatar"
               width={avatarSize}
               height={avatarSize}
             />
           </Avatar>
-          <UserName>Olena</UserName>
+          <UserName>{user.name}</UserName>
         </UserWrap>
 
         <ThemeProvider theme={theme}>
