553a37c1fd720535a4a9295afde99a8ed8ecd018
snizhanamohyl <110724174+snizhanamohyl@users.noreply.github.com>
2023-07-18 02:39:10 +0300

    add getUser

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -4,6 +4,7 @@ export const getServerErrorStatus = state => state.auth.serverErrorStatus;
 export const getResetForm = state => state.auth.resetForm;
 export const getIsLoggedIn = state => state.auth.isLoggedIn;
 export const getIsRefreshing = state => state.auth.isRefreshing;
+export const getUser = state => state.auth.user;
 export const getAllowRefreshUser = state => state.auth.allowRefreshUser;
 
 
