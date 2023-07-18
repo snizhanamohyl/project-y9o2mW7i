96854e9c9c87b265bd5cfb7be3ce2abe9cae6be3
Nataliia Semenova <natasemenova1806@gmail.com>
2023-07-18 17:30:44 +0300

    fix my pecipes page

--- src/components/MyRecipes/MyRecipes.jsx
+++ src/components/MyRecipes/MyRecipes.jsx
@@ -3,54 +3,55 @@ import MyRecipesList from '../MyRecipesList/MyRecipesList';
 import Pagination from '../../components/Pagination/Pagination';
 import { useEffect, useState } from 'react';
 import fetchMyRecipes from '../../services/fetchMyRecipes';
-import { MyRecipesTitle, Container } from './MyRecipes.styled';
-import EmptyPage from '../EmptyPage/EmptyPage';
+import { MyRecipesTitle } from './MyRecipes.styled';
 import { nanoid } from 'nanoid';
+import { useNavigate } from 'react-router';
 
 const MyRecipes = () => {
-    const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const [currentPage, setCurrentPage] = useState(1);
+  const recipesPerPage = 4;
 
-    const recipesPerPage = 4;
+  useEffect(() => {
+    fetchMyRecipes().then(data => {
+      if (data) {
+        setRecipes(data);
+      } else {
+        setRecipes('');
+        navigate('/notFound');
+      }
+    });
+  }, [navigate]);
 
-    useEffect(() => {
-        fetchMyRecipes().then(data => setRecipes(data))
-    }, []);
+  const uniqueKey = nanoid();
 
-    const uniqueKey = nanoid();
+  // індекс останнього рецепту на поточній сторінці
+  //   const lastRecipeIndex = currentPage * recipesPerPage;
+  //   //індекс першого рецепту на поточній сторінці
+  //   const firstRecipeIndex = lastRecipeIndex - recipesPerPage;
+  //   //масив рецептів для поточної сторінки
+  //   const currentRecipes = recipes?.slice(firstRecipeIndex, lastRecipeIndex);
+  console.log(recipes);
+  return (
+    <>
+      <MyRecipesTitle>My recipes</MyRecipesTitle>
+      <MyRecipesList
+        uniqueKey={uniqueKey}
+        isFavorites={false}
+        recipe={recipes}
+      />
+      {recipes.length > 0 ? (
+        <Pagination
+          recipesPerPage={recipesPerPage}
+          totalRecipe={recipes.length}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+      ) : null}
+    </>
+  );
+};
 
-    // індекс останнього рецепту на поточній сторінці
-    const lastRecipeIndex = currentPage * recipesPerPage;
-    //індекс першого рецепту на поточній сторінці
-    const firstRecipeIndex = lastRecipeIndex - recipesPerPage;
-    //масив рецептів для поточної сторінки
-    const currentRecipes = recipes?.slice(firstRecipeIndex, lastRecipeIndex);
-
-
-    return (
-        <>
-            <MyRecipesTitle>My recipes</MyRecipesTitle>
-            {recipes.length > 0 ? (
-                <>
-                    <MyRecipesList uniqueKey={uniqueKey} isFavorites={false} recipe={currentRecipes}/>
-                </>
-            ):(
-                <Container>
-                    <EmptyPage description={"you haven't added your recipes yet"}/>
-                </Container>
-            )}
-    
-            {recipes.length > 0 ? (
-                <Pagination 
-                    recipesPerPage={recipesPerPage} 
-                    totalRecipe={recipes.length} 
-                    currentPage={currentPage}
-                    setCurrentPage={setCurrentPage}
-                /> 
-            ):(null)}
-        </>
-      );
-    };
-    
-    export default MyRecipes;
+export default MyRecipes;
