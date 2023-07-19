fd27c8afee9ce8a00840d497ad587248557ac346
Nataliia Semenova <natasemenova1806@gmail.com>
2023-07-19 22:06:42 +0300

    fix

--- src/components/RecipePageComponents/IngredientsList/IngredientsList.jsx
+++ src/components/RecipePageComponents/IngredientsList/IngredientsList.jsx
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router-dom';
 import RecipePageIngredientsItem from '../IngredientsItem/IngredientsItem';
 import { IngredientsList } from './IngredientsList.styled';
-import { nanoid } from 'nanoid';
 export default function RecipePageIngredientsList({ ingredients }) {
   const { recipeId } = useParams();
 
@@ -10,7 +9,6 @@ export default function RecipePageIngredientsList({ ingredients }) {
       {ingredients?.map(ingredient => {
         const { measure } = ingredient;
         const newId = ingredient.id._id + recipeId;
-      // const newId = nanoid(24)
         const newStructure = {
           ...ingredient.id,
           measure,
